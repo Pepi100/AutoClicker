b264fd962daa4c544fb678acfe3bb7c88f452b02
Pepi100 <radunedelcu02@gmail.com>
2023-08-15 20:47:35 +0300

    MainForm + custom interval

--- src/Main.java
+++ src/Main.java
@@ -1,73 +1,62 @@
 import clickbot.ClickBot;
+import clickbot.ClickType;
 
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.time.Duration;
 
-public class Main {
+public class Main extends JFrame{
 
-//    COLORS
+    //    COLORS
     final static Color bgColor = new Color(43, 45, 48);
     final static Color dangerRed =  new Color(181, 71, 71);
+    final static Color textColor = new Color(222, 224, 208);
 
-    static JFrame mainFrame;
-    static JButton startButton;
-    static  JTextField hours, minutes, seconds, miliseconds ;
 
+    private JButton startButton;
+    private JPanel mainPanel;
+    private JFormattedTextField hours;
+    private JFormattedTextField minutes;
+    private JFormattedTextField seconds;
+    private JFormattedTextField miliseconds;
 
 
     private static boolean state = false;
-    static ClickBot c;
+    private ClickBot c;
+
     public static void main(String[] args) {
-        mainFrameInit();
-
-
+        new Main();
     }
 
 
-    public static void mainFrameInit(){
+    public Main(){
 //        mainFrame.setUndecorated(true);
 //        mainFrame.getRootPane().setWindowDecorationStyle(JRootPane.FRAME);
 
 //        TODO TitleBar
 
 
-        mainFrame=new JFrame();//creating instance of JFrame
-        mainFrame.setTitle("AutoClicker");
-        mainFrame.setSize(400,400);//400 width and 400 height
-
-        JPanel mainPanel = new JPanel();
-        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.PAGE_AXIS));
+        setTitle("AutoClicker");
+        setSize(400,400);//400 width and 400 height
+        setContentPane(mainPanel);
         mainPanel.setBackground(bgColor);
 
-        startButton = new JButton("START");
+
+
+        startButton.setVisible(true);
         startButton.addActionListener(start);
-        mainPanel.add(startButton);
 
-        Box timeBox = new Box(0);
-        hours = new JTextField("Hours");
-        minutes = new JTextField("Minutes");
-        seconds = new JTextField("Seconds");
-        miliseconds = new JTextField("Miliseconds");
 
-        timeBox.add(hours);
-        timeBox.add(minutes);
-        timeBox.add(seconds);
-        timeBox.add(miliseconds);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);//making the frame visible
 
-        mainPanel.add(timeBox);
-
-        mainFrame.add(mainPanel);
-
-        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        mainFrame.setLocationRelativeTo(null);
-        mainFrame.setVisible(true);//making the frame visible
     }
 
 
-    static final ActionListener start = new ActionListener()
+     final ActionListener start = new ActionListener()
     {
         @Override
         public void actionPerformed(ActionEvent e)
@@ -85,8 +74,20 @@ public class Main {
 
             }else{
                 state = true;
-                c= new ClickBot();
+                Duration d=Duration.ZERO;
+                d=d.plusHours(Integer.parseInt(hours.getText()));
+                d=d.plusMinutes(Integer.parseInt(minutes.getText()));
+                d=d.plusSeconds(Integer.parseInt(seconds.getText()));
+                d=d.plusMillis(Integer.parseInt(miliseconds.getText()));
+
+                System.out.println(d);
+//                startButton.setEnabled(false);
+                c= new ClickBot(d, ClickType.LEFT);
+
                 c.start();
+
+
+//                startButton.setEnabled(true);
                 startButton.setText("STOP");
             }
 
@@ -94,4 +95,8 @@ public class Main {
     };
 
 
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
 }
+
